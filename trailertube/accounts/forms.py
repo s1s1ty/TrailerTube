d67da7e7cda6ccf0c9be2d3dbd27541471ea9cdd
s1s1ty <shaonty.dutta@gmail.com>
2016-08-03 19:15:11 +0600

    Update forms.py

--- trailertube/accounts/forms.py
+++ trailertube/accounts/forms.py
@@ -8,6 +8,17 @@ User = get_user_model()
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=200)
     password = forms.CharField(widget=forms.PasswordInput)
+    
+    def clean(self, *args, **kwargs):
+    username = self.cleaned_data.get("username")
+    password = self.cleaned_data.get("password")
+    if username and password:
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise forms.ValidationError("This User doesnot Exist or Incorrect Password.")
+        if not user.is_active:
+            raise forms.ValidationError("This user is no longer active.")
+    return super(LoginForm, self).clean(*args, **kwargs)
 
 class JoinForm(forms.ModelForm):
     email = forms.EmailField()
