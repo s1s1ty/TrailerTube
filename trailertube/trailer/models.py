0c2456d85c1c96e99512dd630949375a244c5d7a
s1s1ty <shaonty.dutta@gmail.com>
2016-08-06 21:07:53 +0600

    Update models.py

--- trailertube/trailer/models.py
+++ trailertube/trailer/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 
+
 def uploaded_location(instance, filename):
     return ("%s/%s") %(instance.title, filename)
 
 class Trailer(models.Model):
     title = models.CharField(max_length=500)
     embed_link = models.TextField()
-    like = models.IntegerField(default=0)
+    approved = models.BooleanField(default=False)
     category = models.CharField(max_length=50)
     release = models.IntegerField(default=2000)
 
     def __str__(self):
         return self.title
+
+class Message(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
 
