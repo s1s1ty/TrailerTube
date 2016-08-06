012af13c84a79bbdc3396a12660b286545dc3515
s1s1ty <shaonty.dutta@gmail.com>
2016-08-06 21:07:02 +0600

    Update admin.py

--- trailertube/trailer/admin.py
+++ trailertube/trailer/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Trailer
+from .models import Trailer, Message
+
+
+def make_approved(modeladmin, request, queryset):
+    queryset.update(approved=True)
 
 class TrailerAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'release', 'like')
+    list_display = ('title', 'category', 'release', 'approved')
+    actions = [make_approved]
 
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message')
 admin.site.register(Trailer, TrailerAdmin)
+admin.site.register(Message, MessageAdmin)
